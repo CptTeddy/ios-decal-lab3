622995cf990661eb0ca4cb104ad597e37f32f502
Jianglai Zhang <jlzhang@berkeley.edu>
2017-02-23 19:51:28 -0800

    Finished lab3.

--- Timer/Timer/ViewController.swift
+++ Timer/Timer/ViewController.swift
@@ -22,36 +22,40 @@ class ViewController: UIViewController {
     
     var timer = Timer()
     @IBOutlet weak var display: UILabel!
-    var startTime = TimeInterval()
-    
-    func updateTime() {
-        var currentTime = Date.timeIntervalSinceReferenceDate
-        var elapsedTime: TimeInterval = currentTime - startTime
-        let minute = elapsedTime / 60.0
-        let second = elapsedTime
-        elapsedTime -= TimeInterval(second)
-        let fract = elapsedTime * 100
-        
-        let double1 = String(format: "%02d", minute)
-        let double2 = String(format: "%02d", second)
-        let double3 = String(format: "%02d", fract)
-        
-        display.text = "\(double1):\(double2):\(double3)"
-        
+    var startTime:Date?
+    func start() {
+        startTime = Date()
     }
     
-//    func increment(_timer: Timer) {
-//        let timeString = String(format: "%02d:%02d.%d", double1, double2, double3)
-//        Stopwatch.text = String(time)
-//    }
+    func updateTime() {
+        var currentTime = -1 * (startTime?.timeIntervalSinceNow)!
+        
+//        var elapsedTime: TimeInterval = currentTime - startTime
+        let minute = Int(floor(currentTime / 60.0))
+        print(minute)
+        let second = Int(round(currentTime).truncatingRemainder(dividingBy: 60))
+//        currentTime -= TimeInterval(second)
+        print(second)
+        let fract = Int((currentTime.truncatingRemainder(dividingBy: 1)) * 100)
+        
+//        let double1 = String(format: "%02d", minute)
+//        let double2 = String(format: "%02d", second)
+//        let double3 = String(format: "%d", fract)
+        print(fract)
+        let timeString = String(format: "%02d:%02d.%d", minute, second, fract)
+        display.text = timeString
+        print(timeString)
+        print(display.text)
+        
+    }
     
     // UIButton with label start. Initiates the timer, update the startTime.
     @IBAction func Start(_ sender: UIButton) {
         let aSelector : Selector = #selector(ViewController.updateTime)
         timer = Timer.scheduledTimer(timeInterval: 0.1, target: self,
-                             selector: aSelector,
-                             userInfo: nil, repeats: true)
-        startTime = Date.timeIntervalSinceReferenceDate
+                                     selector: aSelector,
+                                     userInfo: nil, repeats: true)
+        start()
     }
     
     // UIButton with label stop. Invalidates the timer.
